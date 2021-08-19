f81be45dc3e86a0ead983306ecd655088a7a0f17
Cody Sheridan <sheridan.codyj@gmail.com>
2021-08-19 14:43:54 -0400

    Basic table rendering.

--- mgmt-portal/src/pages/Workbook/WorkbookPage.tsx
+++ mgmt-portal/src/pages/Workbook/WorkbookPage.tsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useTable, Column } from 'react-table';
 
-import { useStyles } from './WorkbookPage.style';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+interface IData {
+  name: string;
+  age: number;
+}
 
 export const WorkbookPage = () => {
-  const classes = useStyles();
+  const columns: Column<IData>[] = useMemo(
+    () => [
+      { Header: 'Name', accessor: 'name' },
+      { Header: 'Age', accessor: 'age' },
+    ],
+    [],
+  );
 
-  return <div>Workbook</div>;
+  const data: IData[] = useMemo(
+    () => [
+      { name: 'Jane', age: 27 },
+      { name: 'Mike', age: 31 },
+    ],
+    [],
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+  return (
+    // apply the table props
+    <TableContainer component={Paper}>
+      <Table aria-label="simple-table" {...getTableProps()}>
+        <TableHead>
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <TableCell style={{ fontWeight: 'bold' }} {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+
+        <TableBody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <TableCell {...cell.getCellProps()}>{cell.render('Cell')}</TableCell>;
+                })}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
