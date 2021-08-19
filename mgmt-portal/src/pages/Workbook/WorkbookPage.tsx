3770674bfc4256eb808f30617114efc39496a1f8
Cody Sheridan <sheridan.codyj@gmail.com>
2021-08-19 15:46:17 -0400

    Editable cells, but UI is incorrect.

--- mgmt-portal/src/pages/Workbook/WorkbookPage.tsx
+++ mgmt-portal/src/pages/Workbook/WorkbookPage.tsx
@@ -9,32 +9,90 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+// Reference: https://codesandbox.io/s/cranky-gauss-806fd?file=/src/App.js:1526-1573
+
 interface IData {
-  name: string;
-  age: number;
+  partNumber: string;
+  jobNumber: number;
+  description: string;
+  operator: string;
+  recvDate: string;
+  stockDate: string;
 }
+
+const EditableCell = ({ value: initialValue, row: { index }, column: { id } }: any) => {
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const onBlur = () => {
+    console.log(value);
+    // updateMyData(index, id, value);
+  };
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return <input value={value} onChange={onChange} onBlur={onBlur} />;
+};
+
+const defaultColumn = {
+  Cell: EditableCell,
+};
 
 export const WorkbookPage = () => {
   const columns: Column<IData>[] = useMemo(
     () => [
-      { Header: 'Name', accessor: 'name' },
-      { Header: 'Age', accessor: 'age' },
+      {
+        Header: 'Sample Machine Name',
+        columns: [
+          { Header: 'Part #', accessor: 'partNumber' },
+          { Header: 'Job #', accessor: 'jobNumber' },
+          { Header: 'Description', accessor: 'description' },
+          { Header: 'Operator', accessor: 'operator' },
+          { Header: 'Recv. Date', accessor: 'recvDate' },
+          { Header: 'Stk. Date', accessor: 'stockDate' },
+        ],
+      },
     ],
     [],
   );
+
+  // Random date for sample.
+  let randDate = new Date(+new Date() - Math.floor(Math.random() * 10000000000)).toLocaleDateString('en-US');
 
   const data: IData[] = useMemo(
     () => [
-      { name: 'Jane', age: 27 },
-      { name: 'Mike', age: 31 },
+      {
+        partNumber: 'ABCD12345',
+        jobNumber: 12345,
+        description: 'Part One',
+        operator: 'Joe',
+        recvDate: '01/01/2021',
+        stockDate: '01/01/2021',
+      },
+      {
+        partNumber: 'EFGH67890',
+        jobNumber: 67890,
+        description: 'Part Two',
+        operator: 'Jane',
+        recvDate: '02/02/2021',
+        stockDate: '02/02/2021',
+      },
     ],
     [],
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+    defaultColumn,
+  });
 
   return (
-    // apply the table props
     <TableContainer component={Paper}>
       <Table aria-label="simple-table" {...getTableProps()}>
         <TableHead>
